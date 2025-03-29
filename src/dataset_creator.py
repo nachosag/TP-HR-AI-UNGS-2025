ff6eb12ded8c5ee09dac4ddd5c7ab99c47ea0d38
nachosag <ignacioborlenghi@gmail.com>
2025-03-29 23:42:22 +0000

    Model created

--- src/dataset_creator.py
+++ src/dataset_creator.py
@@ -1,28 +1,12 @@
 import csv
 import random
 
-# Configuración inicial
 random.seed(42)
-num_entradas = 50
-nombre_archivo = 'candidatos_puesto.csv'
+num_entradas = 5000
 
-# Opciones para cada columna
-niveles_educativos = ['Secundario', 'Universitario', 'Maestría', 'Doctorado']
-habilidades = [
-    'Python', 'Java', 'Gestión de Proyectos', 'Análisis de Datos',
-    'Inglés Avanzado', 'SQL', 'Machine Learning', 'Comunicación',
-    'Trabajo en Equipo', 'Estadística', 'Power BI', 'AWS',
-    'Desarrollo Web', 'Android', 'iOS', 'Francés Básico'
-]
 
-# Función para generar habilidades aleatorias
-def generar_habilidades():
-    num_habilidades = random.randint(2, 5)
-    return ', '.join(random.sample(habilidades, num_habilidades))
-
-# Función para determinar la experiencia del candidato
 def determinar_experiencia(experiencia):
-    if experiencia <= 1:
+    if experiencia == 1:
         return 1
     elif experiencia >= 2 & experiencia <= 5:
         return 3
@@ -31,42 +15,39 @@ def determinar_experiencia(experiencia):
     else:
         return 6
 
-# Función para determinar si es apto
-def determinar_apto(experiencia, educacion, habilidades):
+
+def determinar_apto(experiencia, educacion, habilidad):
     apto = False
-    
+
     puntaje_exp = determinar_experiencia(experiencia)
 
-    puntaje_edu = {
-        'Secundario': 1,
-        'Universitario': 3,
-        'Maestría': 5,
-        'Doctorado': 6
-    }[educacion]
+    puntaje_edu = {"Técnico": 1, "Licenciado": 3, "Ingeniero": 8}[educacion]
 
-    habilidades_clave = {'Python', 'SQL', 'Machine Learning', 'Gestión de Proyectos'}
-    puntaje_hab = len(habilidades_clave.intersection(set(habilidades.split(', ')))) * 2
+    puntaje_hab = 2 if habilidad == 1 else -10
 
-    puntaje_total = puntaje_exp + puntaje_edu + puntaje_hab
-    
-    return 'true' if puntaje_total >= 8 else 'false', puntaje_total
+    puntaje_total = abs(puntaje_exp + puntaje_edu + puntaje_hab) / 10
+
+    return "apto" if puntaje_total >= 0.7 else "no apto", puntaje_total
+
 
 datos = []
 for _ in range(num_entradas):
-    experiencia = random.randint(0,15)
-    educacion = random.choice(niveles_educativos)
-    habilidades_str = generar_habilidades()
-    etiqueta, puntaje_total = determinar_apto(experiencia, educacion, habilidades_str)
-    
-    datos.append([
-        experiencia,
-        educacion,
-        habilidades_str,
-        puntaje_total,
-        etiqueta,
-    ])
+    experiencia = random.randint(1, 15)
+    educacion = random.choice(["Técnico", "Licenciado", "Ingeniero"])
+    habilidad = random.choice([1, 0])
+    aptitud, puntaje = determinar_apto(experiencia, educacion, habilidad)
 
-with open(nombre_archivo, 'w', newline='', encoding='utf-8') as archivo:
+    datos.append(
+        [
+            experiencia,
+            educacion,
+            habilidad,
+            puntaje,
+            aptitud,
+        ]
+    )
+
+with open("dataset.csv", "w", newline="", encoding="utf-8") as archivo:
     writer = csv.writer(archivo)
-    writer.writerow(['Experiencia', 'Educacion', 'Habilidades', 'Puntaje', 'Etiqueta'])
+    writer.writerow(["Experiencia", "Educacion", "Python", "Puntaje", "Aptitud"])
     writer.writerows(datos)
---
+++ src/reader.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from joblib import dump
+
+data = pd.read_csv("./data/dataset.csv")
+data.drop_duplicates(inplace=True)
+
+mms = MinMaxScaler()
+data[["Experiencia", "Puntaje"]] = mms.fit_transform(data[["Experiencia", "Puntaje"]])
+
+ohe = OneHotEncoder(sparse_output=False).set_output(transform="pandas")
+edu_encoded = ohe.fit_transform(data[["Educacion"]])
+
+data["Aptitud"] = data["Aptitud"].map({"apto": 1, "no apto": 0})
+data = pd.concat([data, edu_encoded], axis=1).drop(columns=["Educacion"])
+
+x = data.drop(columns=["Aptitud"])
+y = data["Aptitud"]
+
+# Partición 60% (train) y 40% (test)
+x_train, x_rest, y_train, y_rest = train_test_split(
+    x, y, test_size=0.4, random_state=123
+)
+
+# Partición "rest" en 2 mitades
+x_val, x_test, y_val, y_test = train_test_split(
+    x_rest, y_rest, test_size=0.5, random_state=321
+)
+
+model = LogisticRegression()
+model.fit(x_train, y_train)
+
+print(f"Exactitud promedio entrenamiento: {model.score(x_train,y_train)}")
+print(f"Exactitud promedio validación: {model.score(x_val, y_val)}")
+
+y_pred = model.predict(x_test)
+print("Datos reales:\n", y_test)
+print("Datos predichos:\n", y_pred)
+
+with open("model.joblib", "wb") as f:
+    dump(model, f)
