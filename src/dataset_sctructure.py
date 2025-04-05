47ea9bef7158a4822b380956f47d03e699506b67
nachosag <ignacioborlenghi@gmail.com>
2025-04-05 20:58:41 +0000

    Fix on script.py

---
+++ src/dataset_sctructure.py
@@ -0,0 +1,48 @@
+plantilla = {
+    "Experiencia": 0,
+    "Educación": "",
+    "Área": "",
+    "C# Unity": 0,
+    "C++ Unreal Engine": 0,
+    "Flutter": 0,
+    "HTML": 0,
+    "CSS": 0,
+    "React": 0,
+    "Angular": 0,
+    "JavaScript": 0,
+    "Java": 0,
+    "Kotlin": 0,
+    "Swift": 0,
+    "Python": 0,
+    "SQL": 0,
+    "NoSQL": 0,
+    "Puntos": 0,
+    "Aptitud": "",
+}
+
+areas = {
+    "Desarrollo Web": [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React",
+        "Angular",
+        "SQL",
+        "NoSQL",
+    ],
+    "Desarrollo Móvil": [
+        "Kotlin",
+        "Swift",
+        "Flutter",
+    ],
+    "Desarrollo de Juegos": ["C# Unity", "C++ Unreal Engine", "Python"],
+}
+
+niveles_educativos = ["Ninguna", "Tecnicatura", "Licenciatura", "Ingeniería"]
+
+probabilidad = {
+    "Ninguna": 0.1,
+    "Tecnicatura": 0.2,
+    "Licenciatura": 0.4,
+    "Ingeniería": 0.6,
+}
